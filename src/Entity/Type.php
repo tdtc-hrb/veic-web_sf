f03cf8f611c8971c9b2f3914cdf13e809b079722
tdtc-hrb <tdtc.hrb@gmail.com>
2023-11-05 03:28:19 -0800

    step 4: Company

---
+++ src/Entity/Type.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeRepository::class)]
+class Type
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
